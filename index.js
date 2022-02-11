9db6d9e92dfaa59b2ca96103e511498aa047cc67
steveghana <boatengstephen707@gmail.com>
2022-02-11 00:19:58 +0000

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 class Player {
   constructor() {
+    this.speed = 10;
     this.position = {
       x: 100,
       y: 100,
@@ -19,7 +20,7 @@ class Player {
 
   draw() {
     const { x, y } = this.position;
-    c.fillStyle = "red";
+    // c.fillStyle = "red";
     c.fillRect(x, y, this.width, this.height);
   }
   update() {
@@ -28,39 +29,129 @@ class Player {
     this.position.y += this.velocity.y;
     this.position.x += this.velocity.x;
     const { x, y } = this.position;
+    if(x + this.width + this.velocity.x <= canvas.width) {
+      // console.log(x )
+// alert('hi')
+      // this.velocity.x = 0;
+    }
     if (y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += this.gravity;
-    } else {
-      this.velocity.y = 0;
     }
   }
 }
+const generateImg = (img) => {
+  let image = new Image();
+  image.src = img;
+  return image;
+};
+// let platformImg = new Image()
+// platformImg.src = "./img/platform.png";
+// hills.src = "./img/hills.png";
 //
 class Platform {
-  constructor() {
+  constructor({ x, y, Image }) {
     this.position = {
-      x: 200,
-      y: 300,
+      x,
+      y,
     };
-    this.width = 500;
-    this.height = 50;
+    this.Image = Image;
+    this.width = this.Image.width;
+    this.height = this.Image.height;
   }
   draw() {
-    const { x, y } = this.position;
-    c.fillStyle = "green";
-    c.fillRect(x, y, this.width, this.height);
+    c.drawImage(this.Image, this.position.x, this.position.y);
   }
 }
-
+class GenericScene {
+  constructor({ x, y, Image }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.Image = Image;
+    this.width = this.Image.width;
+    this.height = this.Image.height;
+  }
+  draw() {
+    c.drawImage(this.Image, this.position.x, this.position.y);
+  }
+}
+let platformimg = generateImg("./img/platform.png");
 let player = new Player();
-let platforms = [new Platform()];
+let platforms = [
+  new Platform({ x: -1, y: 610, Image: platformimg }),
+  new Platform({
+    x: platformimg.width * 2 + 100,
+    y: 610,
+    Image: platformimg,
+  }),
+  new Platform({
+    x: platformimg.width * 2 + 200,
+    y: 610,
+    Image: platformimg,
+  }),
+];
+let genericScene = [
+  new GenericScene({
+    x: 0,
+    y: 0,
+    Image: generateImg("./img/background.png"),
+  }),
+  new GenericScene({
+    x: -1,
+    y: 143,
+    Image: generateImg("./img/hills.png"),
+  }),
+];
+const restart = () => {
+  platformimg = generateImg("./img/platform.png");
+  player = new Player();
+  platforms = [
+    new Platform({ x: -1, y: 610, Image: platformimg }),
+    new Platform({
+      x: platformimg.width * 2 + 100,
+      y: 610,
+      Image: platformimg,
+    }),
+    new Platform({
+      x: platformimg.width * 2 + 200,
+      y: 610,
+      Image: platformimg,
+    }),
+  ];
+  genericScene = [
+    new GenericScene({
+      x: 0,
+      y: 0,
+      Image: generateImg("./img/background.png"),
+    }),
+    new GenericScene({
+      x: -1,
+      y: 143,
+      Image: generateImg("./img/hills.png"),
+    }),
+  ];
+};
+let scrolloffset = 0
 function init() {
   requestAnimationFrame(init);
-  c.clearRect(0, 0, canvas.width, canvas.height);
+  // c.fillStyle = "blue";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  genericScene.forEach((scene) => scene.draw());
   player.update();
   platforms.forEach((platform) => platform.draw());
-  //platform collision
+  //illusion of moving platforms
+  if (customkeys.right.pressed) {
+    scrolloffset +=1
+    // platforms.forEach((platform) => (platform.position.x -= player.speed));
+    // genericScene.forEach((scene) => (scene.position.x -= player.speed * 0.66));
+  } else if (customkeys.left.pressed) {
+    scrolloffset -=1
+    // platforms.forEach((platform) => (platform.position.x += player.speed));
+    // genericScene.forEach((scene) => (scene.position.x += player.speed * 0.66));
+  }
   platforms.forEach((platform) => {
+    //platform collision
     if (
       player.position.y + player.height <= platform.position.y &&
       player.position.y + player.height + player.velocity.y >=
@@ -70,26 +161,26 @@ function init() {
     ) {
       player.velocity.y = 0;
     }
-    if (player.position.x >= platform.position.x + platform.width) {
-      player.position.y += player.velocity.y;
-    }
-    if (
-      customkeys.right.pressed &&
-      player.position.x + player.width < innerWidth / 2 - 300
-    ) {
-      player.velocity.x = 7;
-    } else if (customkeys.left.pressed && player.position.x > 100) {
-      player.velocity.x = -7;
-    } else {
-      player.velocity.x = 0;
-      //illusion of moving platforms
-      if (customkeys.right.pressed) {
-        platform.position.x -= 7;
-      } else if (customkeys.left.pressed) {
-        platform.position.x += 7;
-      }
-    }
   });
+
+  if (
+    customkeys.right.pressed &&
+    player.position.x + player.width < innerWidth / 2 - 100
+  ) {
+    player.velocity.x = player.speed;
+  } else if (
+    (customkeys.left.pressed &&
+    player.position.x > 100 ) /* || customkeys.left.pressed && player.position.x > 0 */
+  ) {
+    player.velocity.x = -player.speed;
+  } 
+  
+  else {
+    player.velocity.x = 0;
+  }
+  if (player.position.y > canvas.height) {
+    restart();
+  }
 }
 const customkeys = {
   right: {
@@ -102,7 +193,7 @@ const customkeys = {
 addEventListener("keydown", ({ keyCode }) => {
   switch (keyCode) {
     case 87:
-      player.velocity.y -= 60;
+      player.velocity.y -= 30;
       break;
     case 65:
       console.log("left");
